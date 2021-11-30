2f3f7ddcf1d6fbc15c520b5580eba7a49437756b
opilane <opilane@HP-CZC6297B9X>
2021-11-30 14:02:54 +0200

    123

--- module1.py
+++ module1.py
@@ -1,10 +1,20 @@
-def adding(palk,inimesed):
+def loe_failist_listisse(file:str)->list:
+    """Loeme tekst failist ja salvesta järjendisse
+    """
+    file=open(file,"r")
+    list_=[]
+    for stroka in file:
+        list_.append(stroka.strip())
+    file.close()
+    return list_
+
+def lisa(palk,inimesed):
     """ Добавляет человека и его зарплату
     """
-    add=input("Sissesta nimi=>")
-    inimesed.append(add)
-    add_palk=int(input("Sisesta palk=>"))
-    palk.append(add_palk)
+    a=input("Sissesta nimi=>")
+    inimesed.append(a)
+    b=int(input("Sisesta palk=>"))
+    palk.append(b)
     return palk,inimesed
 
 def otsing_nimi_jargi(inimesed:list,palk:list):
@@ -20,45 +30,51 @@ def otsing_nimi_jargi(inimesed:list,palk:list):
                 palk=palk[k]
                 b+=k+1
                 t.append(nimi+str(palk))
-                #print(nimi, palk)
+                print(nimi, palk)
         else:
             t="Ei ole nimi kirjas"
     return t
 
-def maksimum(palk,inimesed):
-    max_palk=palk[0]
-    kellel=inimesed[0]
-    for p in palk:
-        if p>max_palk:
-            max_palk=p
-            i=palk.index(max_palk)
-            kellel=inimesed[i]
-    return max_palk, kellel   
-
-def minimum(palk,inimesed):
-    min_palk=palk[0]
-    kellel=inimesed[0]
-    for p in palk:
-        if p<min_palk:
-            min_palk=p
-            i=palk.index(min_palk)
-            kellel=inimesed[i]
-    return min_palk, kellel    
-    
-def keskmine(palk):
-    summa=0
-    n=len(palk)
-    for p in palk:
-        summa+=p
-    summa=summa/n
-    return summa
-
-def loe_failist_listisse(file:str)->list:
-    """Loeme tekst failist ja salvesta järjendisse
+def suurim(i:list,p:list):
     """
-    file=open(file,"r")
-    list_=[]
-    for stroka in file:
-        list_.append(stroka.strip())
-    file.close()
-    return list_
+    :rtype: str,str:
+    """
+    suurim=max(p)
+    b=p.index(suurim)
+    kellel=i[b]
+    return suurim , kellel
+
+def keskmine(i:list,p:list):
+    """Keskmise palka leidmine. Kui ta on loetelus, siis näiame kes saab seda kätte
+    :rtype var:
+    """
+    summa=0
+    for palk in p:
+        summa+=p
+    kesk=summa/len(p)
+    print(kesk)
+    vahe=0
+    if 0<=p.index(kesk)<len(p)-1:
+        kesk=i[p.index(kesk)]
+        return kesk
+    else:
+        kesk="Puudub"
+        return kesk
+
+"""
+def sur():
+    palgad=[]
+    with open("palk.txt" , "r") as f1:
+        for stro in f1:
+            palgad.append(stro.strip())
+    f=open("inimesed.txt" , "r")
+    inimesed=[]
+    for stroka in f:
+        inimesed.append(stroka.strip())
+    f.close()
+    palgad_c=palgad.copy()
+    palgad_c.sort()
+    a=palgad_c[0]
+    b=palgad.index(a)
+    print(palgad_c)
+    return a , inimesed[b]"""
--- palgadNikita.py
+++ palgadNikita.py
@@ -1,31 +1,31 @@
 from module1 import *
 from keyboard import *
-palk=loe_failist_listisse("palk.txt")
+palgad=loe_failist_listisse("palk.txt")
 inimesed=loe_failist_listisse("inimesed.txt")
 
 while 1:
-    print("*"*100, "\nKeskmine - A\nMinimum - B\nMaksimum -C\nOtsing -G\nLisa - V")
+    print("*"*100, "\nKeskmine - 1\nMinimum - 2\nMaksimum -3\nOtsing -4\nLisa - 5")
     print("Vajuta nuppu==>")
 
-    if read_key()=="A":
-        kesk_palk=round(keskmine(palk),2)
-        print("Keskmine palk on ",kesk_palk)
+    if read_key()=="1":
+        summa=keskmine(inimesed,palgad)
+        print("Keskmine palk on ",summa)
 
-    elif read_key()=="B":
+    elif read_key()=="2":
         min_palk,kellel=minimum(palk,inimesed)
         print("Minimaalne palk=> ", min_palk, " Kellel=> ",kellel)
     
-    elif read_key()=="C":
-        max_palk,kellel=maksimum(palk,inimesed)
-        print("Maksimaalne palk=> ", max_palk, " Kellel=> ",kellel)
+    elif read_key()=="3":
+        suurim,kellel=suurim(inimesed,palgad)
+        print("Maksimaalne palk=> ", suurim, " Kellel=> ",kellel)
     
-    elif read_key()=="G":
-        vastus=otsing_nimi_jargi(inimesed, palk)
+    elif read_key()=="4":
+        vastus=otsing_nimi_jargi(inimesed, palgad)
         print(vastus)
 
-    elif read_key()=="V":
-        i=adding(palk,inimesed)
-        print(palk,inimesed)
+    elif read_key()=="5":
+        palgad,inimesed=lisa(palgad,inimesed)
+        print(palgad,inimesed)
 
 
 
