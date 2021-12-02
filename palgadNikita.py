1e0381bc76a8804c2ab7cddfbc6f74af1b2ccb07
opilane <opilane@HP-CZC6297B9X>
2021-12-02 08:49:04 +0200

    12

--- palgadNikita.py
+++ palgadNikita.py
@@ -4,26 +4,22 @@ palgad=loe_failist_listisse("palk.txt")
 inimesed=loe_failist_listisse("inimesed.txt")
 
 while 1:
-    print("*"*100, "\nKeskmine - 1\nMinimum - 2\nMaksimum -3\nOtsing -4\nLisa - 5")
+    print("*"*100, "\nKeskmine - 1\nMaksimum -2\nOtsing -3\nLisa - 4")
     print("Vajuta nuppu==>")
 
     if read_key()=="1":
         kesk=keskmine(inimesed,palgad)
         print("Keskmine palk on ",summa)
-
-    elif read_key()=="2":
-        min_palk,kellel=minimum(palk,inimesed)
-        print("Minimaalne palk=> ", min_palk, " Kellel=> ",kellel)
     
-    elif read_key()=="3":
+    elif read_key()=="2":
         suurim,kellel=suurim(inimesed,palgad)
         print("Maksimaalne palk=> ", suurim, " Kellel=> ",kellel)
     
-    elif read_key()=="4":
+    elif read_key()=="3":
         vastus=otsing_nimi_jargi(inimesed, palgad)
         print(vastus)
 
-    elif read_key()=="5":
+    elif read_key()=="4":
         palgad,inimesed=lisa(palgad,inimesed)
         print(palgad,inimesed)
 
